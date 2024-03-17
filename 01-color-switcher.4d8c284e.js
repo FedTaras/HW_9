dc950153c97db395b382961a33b7e9c29b961c8f
FedTaras <113994712+FedTaras@users.noreply.github.com>
2024-03-17 17:38:34 +0000

    Deploying to gh-pages from @ FedTaras/HW_9@3814755b437d86d25b607310389e5a01896c7c4b 🚀

---
+++ 01-color-switcher.4d8c284e.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};t.startBtn.addEventListener("click",(function(){id=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(id),t.stopBtn.disabled=!0,t.startBtn.disabled=!1})),t.stopBtn.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.4d8c284e.js.map
