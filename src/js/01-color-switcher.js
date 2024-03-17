3814755b437d86d25b607310389e5a01896c7c4b
FedTaras <113994712+FedTaras@users.noreply.github.com>
2024-03-17 19:38:12 +0200

    First task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,32 @@
+const ref = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+// let id = null;
+
+ref.startBtn.addEventListener('click', changeColor);
+ref.stopBtn.addEventListener('click', stopFn);
+
+ref.stopBtn.disabled = true;
+
+function changeColor() {
+  id = setInterval(() => {
+    ref.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  ref.startBtn.disabled = true;
+  ref.stopBtn.disabled = false;
+}
+
+function stopFn() {
+  clearInterval(id);
+  ref.stopBtn.disabled = true;
+  ref.startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
